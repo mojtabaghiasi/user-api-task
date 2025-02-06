f8dba00d8222473708a679185d981499d972f9b0
mojtaba <mojtabagiasi@gmail.com>
2025-02-06 13:24:31 +0000

    chore: implement user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsJWT,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsEnum({ ADMIN: 'ADMIN', USER: 'USER' }, { message: 'Valid Role Required' })
+  role: 'ADMIN' | 'USER';
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  @MaxLength(20, { message: 'Password must not exceed 20 characters' })
+  password: string;
+  @IsString()
+  @IsJWT()
+  accessToken: string;
+}
