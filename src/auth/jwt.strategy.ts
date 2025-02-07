d3548f33dd1d727d804c0e97af1d3a468358921a
mojtaba <mojtabagiasi@gmail.com>
2025-02-07 10:54:03 +0000

    chore: add missing jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from './constants';
+import { UserEntity } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  validate(payload: UserEntity) {
+    return {
+      id: payload.id,
+      email: payload.email,
+      name: payload.name,
+      role: payload.role,
+    };
+  }
+}
