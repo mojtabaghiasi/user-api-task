2446c956a0621cf57dbfd6075b3ec6b29823bccb
mojtaba <mojtabagiasi@gmail.com>
2025-02-07 15:03:20 +0000

    fix: fix undefined user error

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -9,7 +9,7 @@ import { Request } from 'express';
 import { UserEntity } from '../../users/entities/user.entity';
 import { ROLES_KEY } from '../decorators/has-roles.decorator';
 import { UsersService } from '../../users/users.service';
-import { Role } from '@prisma/client';
+import { UserRoles } from '../../users/entities/user.roles';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -19,17 +19,12 @@ export class RolesGuard implements CanActivate {
   ) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles = this.reflector.getAllAndOverride<UserRoles[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
     if (!requiredRoles) {
       return true;
-    }
-
-    const hasRequiredRole = requiredRoles.some((role) => user?.role === role);
-    if (!hasRequiredRole) {
-      throw new ForbiddenException('You are not authorized.');
     }
 
     const request = context
@@ -38,7 +33,12 @@ export class RolesGuard implements CanActivate {
     const user = request.user;
     const userIdFromParams = Number(request.params.id);
 
-    if (user.role === 'USER') {
+    const hasRequiredRole = requiredRoles.some((role) => user?.role === role);
+    if (!hasRequiredRole) {
+      throw new ForbiddenException('You are not authorized.');
+    }
+
+    if (user.role === UserRoles.USER) {
       if (
         (request.method === 'GET' || request.method === 'PATCH') &&
         userIdFromParams === user.id
@@ -49,7 +49,7 @@ export class RolesGuard implements CanActivate {
         'You are not authorised to perform this action.',
       );
     }
-    if (user.role === 'ADMIN') {
+    if (user.role === UserRoles.ADMIN) {
       if (request.method === 'DELETE') {
         if (user.id === userIdFromParams) {
           throw new ForbiddenException('You cannot delete yourself.');
