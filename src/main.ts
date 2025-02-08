45f0d1b24ae017053a4dc29d132a32ea88129d5d
mojtaba <mojtabagiasi@gmail.com>
2025-02-08 10:29:15 +0000

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'node:fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('User API')
+    .setDescription('The users API documentation')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  fs.writeFileSync(
+    './swagger-spec.json',
+    JSON.stringify(documentFactory(), null, 2),
+  );
+  SwaggerModule.setup('api', app, documentFactory);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -8,15 +8,20 @@ import {
 } from 'class-validator';
 import { UserEntity } from '../entities/user.entity';
 import { UserRoles } from '../entities/user.roles';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto extends UserEntity {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
+  @ApiProperty()
   @IsEmail()
   email: string;
+  @ApiProperty()
   @IsEnum({ ADMIN: 'ADMIN', USER: 'USER' }, { message: 'Valid Role Required' })
   role: UserRoles;
+  @ApiProperty()
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
   @MaxLength(20, { message: 'Password must not exceed 20 characters' })
